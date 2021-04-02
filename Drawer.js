ddaf37c01398e6249226328299398ca7ad1e0b5e
Mohammad Saad <81032267+masaad01@users.noreply.github.com>
2021-04-02 21:06:21 +0300

    Added more functionality for drawer
    added undo/redo/clear and snap to grid option
    
    in the css changed the grid of the control section

--- Drawer.js
+++ Drawer.js
@@ -1,5 +1,23 @@
 let count=0;
-let mousePos = {x: 0, y: 0};
+let mousePos = {x:0 , y:0};
+let gridCell = 30;
+let gridFlag = true;
+Object.defineProperties(mousePos,{
+    xGrid:{
+        get: function(v){
+            if(gridCell <= 0 || !gridFlag)
+                return this.x;
+            return Math.round(this.x/gridCell)*gridCell;
+        }
+    },
+    yGrid:{
+        get: function(v){
+            if(gridCell <= 0 || !gridFlag)
+                return this.y;
+            return Math.round(this.y/gridCell)*gridCell;
+        }
+    }
+});
 
 function Line(ctx){
     this.startPos = {x: undefined,y: undefined};
@@ -62,7 +80,8 @@ function Circle(ctx){
     }
 }
 function Drawer(ctx){
-    this.shapes = [];
+    let shapes = [];
+    let deleted = [];
     this.demoStartP = undefined;
     this.type = undefined;
     const typeConstructor = {
@@ -71,16 +90,20 @@ function Drawer(ctx){
         Circle: () => new Circle(ctx)
     }
     
+    Object.defineProperties(this,{
+        shapes: {get: () => shapes}
+    })
+
     this.penDown = function(startP){
-        this.shapes.push(typeConstructor[this.type]()); 
+        shapes.push(typeConstructor[this.type]()); 
         
-        let l = this.shapes.length;       
-        this.shapes[l-1].startPos = startP;
+        let l = shapes.length;       
+        shapes[l-1].startPos = startP;
     }
     this.penUp = function(endP){
-        let l = this.shapes.length;
-        this.shapes[l-1].endPos = endP;
-        this.shapes[l-1].draw();
+        let l = shapes.length;
+        shapes[l-1].endPos = endP;
+        shapes[l-1].draw();
     }
     this.demo = function(startP,endP = mousePos){
         let shape = typeConstructor[this.type]();
@@ -89,11 +112,28 @@ function Drawer(ctx){
         shape.endPos = endP;
         shape.draw();   
     }
+    this.undo = function(){
+        if(shapes.length > 0)
+            deleted.push(shapes.pop());
+    }
+    this.redo = function(){
+        if(deleted.length > 0)
+            shapes.push(deleted.pop());
+    }
+    this.reset = function(){
+        shapes = [];
+        deleted = [];
+        this.demoStartP = undefined;
+        this.type = undefined;
+    }
 }
 function DrawerGUI(){
     //private vars
     let canvas , mainSection,ctx;
-    const input = {type: undefined};
+    const input = {
+        type: undefined, undo: undefined , redo: undefined, 
+        reset: undefined, grid:{size: undefined, flag: undefined}
+    };
     //public vars
     this.app = "";
     //private methods
@@ -106,11 +146,28 @@ function DrawerGUI(){
         let scoreSection = htmlCreator("div",container,"scoreSec","view");
         let controlsSection = htmlCreator("div",container,"controlsSec","view");
 
-        htmlCreator("label",controlsSection,"","","Shape: ");
-        input.type = htmlCreator("select",controlsSection,"","inputList")
+        htmlCreator("label",controlsSection,"","gridSpan6","click and drag to draw");
+
+        htmlCreator("label",controlsSection,"","gridSpan2","Shape: ");
+        input.type = htmlCreator("select",controlsSection,"","inputList gridSpan4")
         htmlCreator("option",input.type,"","","Line");
         htmlCreator("option",input.type,"","","Rectangle");
         htmlCreator("option",input.type,"","","Circle");
+        
+        htmlCreator("label", controlsSection, "", "gridSpan2", "Snap: ").title = "snap pen to grid";
+        input.grid.flag = htmlCreator("input", controlsSection, "","inputCheckbox ");
+        input.grid.flag.type = "checkbox";
+        input.grid.flag.checked = gridFlag;
+        input.grid.size = htmlCreator("input", controlsSection, "","inputNumber gridSpan2");
+        input.grid.size.type = "number";    
+        input.grid.size.min = "10";
+        input.grid.size.max = "100";
+        input.grid.size.value = gridCell;
+        htmlCreator("label", controlsSection, "", "", "px");
+
+        input.undo = htmlCreator("button",controlsSection,"","button gridSpan3","Undo");
+        input.redo = htmlCreator("button",controlsSection,"","button gridSpan3","Redo");
+        input.reset = htmlCreator("button",controlsSection,"","button gridSpan6","Clear All");
 
         canvas = htmlCreator("canvas" ,mainSection);
         canvas.height = mainSection.offsetHeight ;
@@ -125,18 +182,18 @@ function DrawerGUI(){
             mousePos.y = event.y;
             if(self.app.demoStartP != undefined){
                 self.display(0);
-                self.app.demo(self.app.demoStartP,{x:event.x,y:event.y});
+                self.app.demo(self.app.demoStartP,{x:mousePos.xGrid,y:mousePos.yGrid});
             }
         });
         canvas.addEventListener("mousedown", function(event){
             if(event.button == 0){   //left mouse button
-                self.app.penDown({x:event.x,y:event.y});
-                self.app.demoStartP = {x:event.x,y:event.y};
+                self.app.penDown({x:mousePos.xGrid,y:mousePos.yGrid});
+                self.app.demoStartP = {x:mousePos.xGrid,y:mousePos.yGrid};
             }
         });
         window.addEventListener("mouseup", function(event){
             if(self.app.demoStartP != undefined && event.button == 0){
-                self.app.penUp({x:event.x,y:event.y});
+                self.app.penUp({x:mousePos.xGrid,y:mousePos.yGrid});
                 self.app.demoStartP = undefined;
             }
         });
@@ -144,20 +201,32 @@ function DrawerGUI(){
             canvas.height = mainSection.offsetHeight ;
             canvas.width = mainSection.offsetWidth;
             self.display(0);
-        })
-        // canvas.addEventListener("mouseout", function(event){
-        //     if(self.app.demoStartP != undefined){
-        //         self.app.penUp({x:event.x,y:event.y});
-        //         self.app.demoStartP = undefined;
-        //     }
-        // });
+        });
         input.type.addEventListener("change",function(){self.app.type = this.value;})
+        input.undo.addEventListener("click", function(){self.app.undo();self.display.bind(self)(0);});
+        input.redo.addEventListener("click", function(){self.app.redo();self.display.bind(self)(0);});
+        input.reset.addEventListener("click", function(){self.reset();});
+        input.grid.size.addEventListener("change",function(){gridCell = this.value;self.display.bind(self)(0);})
+        input.grid.flag.addEventListener("change",function(){gridFlag = this.checked;self.display.bind(self)(0);})
     }
     const clearCanvas = function(){
+        let size = {x: canvas.width/gridCell , y: canvas.height/gridCell};
         ctx.clearRect(0,0,canvas.width,canvas.height);
-                
         
-
+        ctx.globalAlpha = 0.2;
+        for(let i=0;i<size.y+1;i++){
+            ctx.beginPath();
+            ctx.moveTo(0, i * gridCell);
+            ctx.lineTo(gridCell * size.x, i * gridCell);
+            ctx.stroke();
+        }
+        for(let i=0;i<size.x+1;i++){
+            ctx.beginPath();
+            ctx.moveTo(i * gridCell, 0);
+            ctx.lineTo(i * gridCell, gridCell * size.y);
+            ctx.stroke();
+        }
+        ctx.globalAlpha = 1;
     }
     //public methods
     this.start = function(){
@@ -166,7 +235,6 @@ function DrawerGUI(){
 
         
         this.app = new Drawer(ctx);
-        this.app.type = input.type.value;
         clearCanvas.bind(this)();
         addEvents.bind(this)();
         this.reset();
@@ -174,13 +242,12 @@ function DrawerGUI(){
     this.display = function(){
         clearCanvas.bind(this)();
         for(let i=0,l=this.app.shapes.length;i<l;i++)
-            this.app.shapes[i].draw();
-
-        
+            this.app.shapes[i].draw();        
     }
     this.reset = function(){
-        //this.app.initialize();
-
+        this.app.reset();
+        this.app.type = input.type.value;
+        this.display(0);
     }
 }
 
@@ -201,7 +268,4 @@ function htmlCreator(tag,parent,id="",clss="",inHTML=""){
     t.innerHTML = inHTML;
 
     return t;
-}
-function fun(c=""){
-    alert(c +"\n"+ count++);
 }
